f8b7cc1048e2e2cb1aca746098d9544be4227df6
Celso Furtado <celsofurtado@terra.com.br>
2022-08-23 18:58:15 -0300

    Somente GUI

---
+++ app/src/main/java/br/com/fiap/gameslistprof/AddUserActivity.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.gameslistprof
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_user)
+    }
+}
